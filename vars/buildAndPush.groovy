21846eb485848177e379cbd27761409880559db0
thrucker <t.rucker@xpure.de>
2019-04-05 14:27:49 +0200

    Separate `buildAndPush` from helper functions
    * introduce the `gitops` global variable to be able to use the helper
    functions individually

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,25 +1,4 @@
-def imageRef(name) {
-  String imageName = "${env.DOCKER_REGISTRY}/${env.GOOGLE_PROJECT}/${name}"
-  String tag
-
-  if (env.BRANCH_NAME == "master") {
-    tag = "1.0.0-${env.BUILD_NUMBER}"
-  } else {
-    tag = "${env.BRANCH_NAME}-${env.GIT_COMMIT}"
-  }
-
-  return "${imageName}:${tag}"
-}
-
-def buildImage(name, buildArgs) {
-  sh "docker build -t ${imageRef(name)} ${buildArgs} ."
-}
-
-def pushImage(name) {
-  sh "docker push ${imageRef(name)}"
-}
-
 def call(name, buildArgs = "") {
-  buildImage(name, buildArgs)
-  pushImage(name)
+  gitops.buildImage(name, buildArgs)
+  gitops.pushImage(name)
 }
---
+++ vars/gitops.groovy
@@ -0,0 +1,20 @@
+def imageRef(name) {
+  String imageName = "${env.DOCKER_REGISTRY}/${env.GOOGLE_PROJECT}/${name}"
+  String tag
+
+  if (env.BRANCH_NAME == "master") {
+    tag = "1.0.0-${env.BUILD_NUMBER}"
+  } else {
+    tag = "${env.BRANCH_NAME}-${env.GIT_COMMIT}"
+  }
+
+  return "${imageName}:${tag}"
+}
+
+def buildImage(name, buildArgs) {
+  sh "docker build -t ${imageRef(name)} ${buildArgs} ."
+}
+
+def pushImage(name) {
+  sh "docker push ${imageRef(name)}"
+}
