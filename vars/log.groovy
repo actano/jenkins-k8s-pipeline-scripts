66f31df164609f61a089683504ee3a8505176355
thrucker <t.rucker@xpure.de>
2019-04-05 15:36:38 +0200

    Add method for debugging

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,15 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+def imageRef(name) {
+  String imageName = "${env.DOCKER_REGISTRY}/${env.GOOGLE_PROJECT}/${name}"
+  String tag
+
+  if (env.BRANCH_NAME == "master") {
+    tag = "1.0.0-${env.BUILD_NUMBER}"
+  } else {
+    tag = "${env.BRANCH_NAME}-${env.GIT_COMMIT}"
+  }
+
+  return "${imageName}:${tag}"
+}
