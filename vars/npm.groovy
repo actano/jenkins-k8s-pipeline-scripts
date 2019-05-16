c45665329f20f2466cb294f5c65ed1da898319e2
thrucker <t.rucker@xpure.de>
2019-05-16 09:50:06 +0200

    Add script for publishing npm modules

---
+++ vars/npm.groovy
@@ -0,0 +1,18 @@
+def withAuth(String secretPath, String secretKey, Closure body) {
+  def secrets = [
+    [$class: 'VaultSecret', path: secretPath, secretValues: [
+      [$class: 'VaultSecretValue', envVar: 'NPM_TOKEN', vaultKey: secretKey]
+    ]]
+  ]
+
+  wrap([$class: 'VaultBuildWrapper', vaultSecrets: secrets]) {
+    body()
+  }
+}
+
+def publish(String registry = "registry.npmjs.org", String secretPath = "secret/rplanx/npmjs.com/rplan-ci", String secretKey = "publish-token") {
+  withAuth(secretPath, secretKey) {
+    sh "echo \"//${registry}/:_authToken=\\\${NPM_TOKEN}\" > ~/.npmrc"
+    sh "npm publish"
+  }
+}
