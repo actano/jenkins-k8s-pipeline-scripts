1de6a5af3b062645aec9421486c257b1826dc352
thrucker <t.rucker@xpure.de>
2019-05-16 10:23:47 +0200

    Revert "Debug script"
    This reverts commit 9258186137149f73a3e81511f07b9de8a2ca908b.

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -13,8 +13,6 @@ def withAuth(String secretPath, String secretKey, Closure body) {
 def publish(String registry = "registry.npmjs.org", String secretPath = "secret/rplanx/npmjs.com/rplan-ci", String secretKey = "publish-token") {
   withAuth(secretPath, secretKey) {
     sh "echo \"//${registry}/:_authToken=\\\${NPM_TOKEN}\" > ~/.npmrc"
-    sh "cat ~/.npmrc"
-    sh "env"
     sh "npm publish"
   }
 }
