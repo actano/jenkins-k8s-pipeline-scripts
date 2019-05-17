b87750239bda8e6d1409feeeed7068425e0d4c4b
thrucker <t.rucker@xpure.de>
2019-05-17 14:36:06 +0200

    Remove creation of `.npmrc` file
    It should be contained in the respective git repo

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -6,7 +6,6 @@ def withAuth(String registry = "registry.npmjs.org", String secretPath = "secret
   ]
 
   wrap([$class: 'VaultBuildWrapper', vaultSecrets: secrets]) {
-    sh "echo \"//${registry}/:_authToken=\\\${NPM_TOKEN}\" > ~/.npmrc"
     body()
   }
 }
