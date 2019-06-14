f6877d9ac1fd357a39fe42b9293379a2621d33b1
Christoph Vitz <c.vitz@itsonix.eu>
2019-06-14 10:44:55 +0200

    Revert "Add logging of env variables"
    This reverts commit dd767d677130773dc4ed28c63f10667eefe0cf0a.

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,8 +1,4 @@
 def withAuth(String registry = "registry.npmjs.org", String secretPath = "secret/rplanx/npmjs.com/rplan-ci", String secretKey = "publish-token", Closure body) {
-
-  sh "echo env in npm helper"
-  sh "env"
-
   def secrets = [
     [$class: 'VaultSecret', path: secretPath, secretValues: [
       [$class: 'VaultSecretValue', envVar: 'NPM_TOKEN', vaultKey: secretKey]
