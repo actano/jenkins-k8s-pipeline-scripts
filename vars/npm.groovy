b3675698ff092f259f8ab6dcce1d493777275708
thrucker <t.rucker@xpure.de>
2019-06-17 16:29:03 +0200

    Specify vault engine version

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,6 +1,6 @@
 def withAuth(String registry = "registry.npmjs.org", String secretPath = "secret/rplanx/npmjs.com/rplan-ci", String secretKey = "publish-token", Closure body) {
   def secrets = [
-    [$class: 'VaultSecret', path: secretPath, secretValues: [
+    [$class: 'VaultSecret', engineVersion: 1, path: secretPath, secretValues: [
       [$class: 'VaultSecretValue', envVar: 'NPM_TOKEN', vaultKey: secretKey]
     ]]
   ]
