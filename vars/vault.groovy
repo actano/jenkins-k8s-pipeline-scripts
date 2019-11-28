7f1fcf43ea3e3e83f51a9ca56f189f285be72591
Wasiq Kashkari <w.kashkari@outlook.com>
2019-11-28 17:01:19 +0100

    PA-2656: Rename withVaultSecrets to withVaultAddressAndToken

--- vars/vault.groovy
+++ vars/vault.groovy
@@ -1,4 +1,4 @@
-def withVaultSecrets(secretPath = "secret/rplanx/jenkins", Closure body) {
+def withVaultAddressAndToken(secretPath = "secret/rplanx/jenkins", Closure body) {
   def secrets = [
     [$class: 'VaultSecret', engineVersion: 1, path: secretPath, secretValues: [
       [$class: 'VaultSecretValue', envVar: 'VAULT_ADDR', vaultKey: 'vault-address'],
