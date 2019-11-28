d0010211396a99a172d93661ab9d94cb6277fe33
Wasiq Kashkari <w.kashkari@outlook.com>
2019-11-28 15:44:51 +0100

    PA-2656: Add a vault plugin so pact-broker-helper can be run easily

---
+++ vars/vault.groovy
@@ -0,0 +1,12 @@
+def withVault(secretPath = "secret/rplanx/jenkins", Closure body) {
+  def secrets = [
+    [$class: 'VaultSecret', engineVersion: 1, path: secretPath, secretValues: [
+      [$class: 'VaultSecretValue', envVar: 'VAULT_ADDR', vaultKey: 'vault-address'],
+      [$class: 'VaultSecretValue', envVar: 'VAULT_TOKEN', vaultKey: 'vault-token'],
+    ]]
+  ]
+
+  wrap([$class: 'VaultBuildWrapper', vaultSecrets: secrets]) {
+    body()
+  }
+}
