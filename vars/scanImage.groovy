7938a428e48f73a3cb50b3dbfa4ce55f889bd7e8
thrucker <t.rucker@itsonix.eu>
2021-02-03 14:37:10 +0100

    PA-8584: Fix variable name

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,7 +1,7 @@
 def call(imageName) {
     String img = gitops.imageRef(imageName)
     String tag = gitops.imageTag()
-    String scanResultFile = "scan-${poServiceImageName}-${tag}.txt"
+    String scanResultFile = "scan-${imageName}-${tag}.txt"
     sh "grype \"${img}\" 2>&1 | tee \"${scanResultFile}\""
     sh "gsutil cp \"${scanResultFile}\" \"gs://allex-image-scan-results/${imageName}/${scanResultFile}\""
 }
