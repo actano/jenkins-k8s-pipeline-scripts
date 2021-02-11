34b9bc554bcc92ccf28d71dc04d2c7e3ffc351c2
thrucker <t.rucker@itsonix.eu>
2021-02-11 10:21:24 +0100

    Also push image to new artifact repo

--- vars/gitops.groovy
+++ vars/gitops.groovy
@@ -3,7 +3,13 @@ def imageTag(prefix="${env.BRANCH_NAME}") {
 }
 
 def imageRef(name, prefix="${env.BRANCH_NAME}") {
-  String imageName = "${env.DOCKER_REGISTRY}/${env.GOOGLE_PROJECT}/${name}"
+    String imageName = "${env.DOCKER_REGISTRY}/${env.GOOGLE_PROJECT}/${name}"
+    String tag = imageTag(prefix)
+    return "${imageName}:${tag}"
+}
+
+def imageRefArtifactRepo(name, prefix="${env.BRANCH_NAME}") {
+  String imageName = "europe-west3-docker.pkg.dev/allex-artifacts/allex-artifacts-docker/${name}"
   String tag = imageTag(prefix)
   return "${imageName}:${tag}"
 }
@@ -14,4 +20,6 @@ def buildImage(name, directory=".", buildArgs = "") {
 
 def pushImage(name) {
   sh "docker push ${imageRef(name)}"
+  sh "docker tag ${imageRef(name)} ${imageRefArtifactRepo(name)}"
+  sh "docker push ${imageRefArtifactRepo(name)}"
 }
