3ea66e808011406ec1a9506008e3fe80c264674e
thrucker <t.rucker@itsonix.eu>
2021-03-10 16:32:15 +0100

    PA-8844: Add possibility to specify grype blacklist

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -3,7 +3,7 @@ def call(imageName) {
     String tag = gitops.imageTag()
     String ts = new Date().format("yyyyMMddHHmm")
     String scanResultFile = "scan-${imageName}-${tag}-${ts}.txt"
-    sh "grype \"${img}\" 2>&1 | tee \"${scanResultFile}\""
+    sh "grype \"${img}\" 2>&1 | ([ ! -f \"./grype-blacklist.txt\" ] && cat || grep -v -f grype-blacklist.txt) | tee \"${scanResultFile}\""
     sh "egrep -o \"Critical|High\" \"${scanResultFile}\" | sort | uniq -c | tee -a \"${scanResultFile}\""
     sh "gsutil cp \"${scanResultFile}\" \"gs://allex-image-scan-results/${imageName}/${scanResultFile}\""
 }
