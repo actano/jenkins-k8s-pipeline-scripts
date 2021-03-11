ed906b063464dd53078b550d6c7bc52a89769802
thrucker <t.rucker@itsonix.eu>
2021-03-11 15:20:20 +0100

    Make scanning by image reusable

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,9 +1,14 @@
 def call(imageName) {
-    String img = gitops.imageRef(imageName)
+    String imageRef = gitops.imageRef(imageName)
     String tag = gitops.imageTag()
+    String scanId = "${imageName}-${tag}"
+    byTag(imageRef, imageName, scanId)
+}
+
+def byTag(imageRef, imageName, scanId) {
     String ts = new Date().format("yyyyMMddHHmm")
-    String scanResultFile = "scan-${imageName}-${tag}-${ts}.txt"
-    sh "grype \"${img}\" 2>&1 | ([ ! -f \"./grype-blacklist.txt\" ] && cat || grep -v -f grype-blacklist.txt) | tee \"${scanResultFile}\""
+    String scanResultFile = "scan-${scanId}-${ts}.txt"
+    sh "grype \"${imageRef}\" 2>&1 | ([ ! -f \"./grype-blacklist.txt\" ] && cat || grep -v -f grype-blacklist.txt) | tee \"${scanResultFile}\""
     sh "egrep -o \"Critical|High\" \"${scanResultFile}\" | sort | uniq -c | tee -a \"${scanResultFile}\""
     sh "gsutil cp \"${scanResultFile}\" \"gs://allex-image-scan-results/${imageName}/${scanResultFile}\""
 }
